90705052816317811515a243da6627898350e06f
changho <ggrkdehrud@nate.com>
2014-11-28 03:58:43 +0900

    member bean dao완성, 가입창완성

--- Web_Teamproject/WebContent/signup.jsp
+++ Web_Teamproject/WebContent/signup.jsp
@@ -1,12 +1,146 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+    pageEncoding="UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %> 
+<!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
+	<meta charset="UTF-8">
+	<title>회원가입</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+	<link href="css/mwLogin.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<link href="css/photo.css" rel="stylesheet">	
+	
+	<script src="js/jquery-1.8.2.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/mwLogin.js"></script>
 </head>
 <body>
+ 
+ <jsp:include page="mainheader.jsp" />
+ 
+ <div class="container">
+   <div>
+		  <form class="form-horizontal" action="login" method="POST" enctype="multipart/form-data">
+			<fieldset>
+        <legend class="legend">Sign Up</legend>
+        <c:if test="${method == 'PUT'}">
+          <input type="hidden" name="_method" value="PUT"/>
+          <input type="hidden" name="userid" value="${user.userid }"/>
+        </c:if>
+        <c:if test="${method == 'POST'}">
+          <input type="hidden" name="_method" value="POST"/>
+        </c:if>
 
+				<div class="control-group">
+					<label class="control-label" for="userid">ID</label>
+					<div class="controls">
+						<c:choose>
+							<%-- 신규 가입일 때만 아이디 입력창이 나타남--%>
+	        				<c:when test="${method == 'POST'}">
+								<input type="text" name="userid">
+							</c:when>
+							<c:otherwise>
+								<c:out value="${user.userid}"/>
+							</c:otherwise>
+						</c:choose>
+					</div>
+				</div>
+
+				<div class="control-group">
+					<label class="control-label" for="pwd">비밀번호</label>
+					<div class="controls">
+						<input type="password" name="pwd">
+					</div>
+				</div>
+
+				<div class="control-group">
+					<label class="control-label" for="pwd_confirm">비밀번호 확인</label>
+					<div class="controls">
+						<input type="password" name="pwd_confirm">
+					</div>
+				</div>
+
+				<div class="control-group">
+					<label class="control-label" for="lastname">성</label>
+					<div class="controls">
+						<input type="text" placeholder="홍" name="lastname" value="${user.lastname}">
+					</div>
+				</div>
+
+				<div class="control-group">
+					<label class="control-label" for="firstname">이름</label>
+					<div class="controls">
+						<input type="text" placeholder="길동" name="firstname" value="${user.firstname}">
+					</div>
+				</div>
+
+				<div class="control-group">
+					<label class="control-label" for="nickname">닉네임</label>
+					<div class="controls">
+						<input type="text" placeholder="닉네임" name="nickname" value="${user.nickname}">
+					</div>
+				</div>
+
+				<div class="control-group">
+					<label class="control-label" for="email">E-mail</label>
+					<div class="controls">
+						<input type="email" placeholder="abc@naver.com" name="email" value="${user.email}">
+					</div>
+				</div>
+
+				<div class="control-group">
+					<label class="control-label">성별</label>
+					<div class="controls">
+					  <c:forEach var="gender" items="${user.genders}">
+					    <label class="radio">
+					      <input type="radio" value="${gender[0]}" name="gender" ${user.checkGender(gender[0])}/>
+					      ${gender[1]}
+					    </label>
+					  </c:forEach>
+					</div>
+				</div>
+
+				<div class="control-group">
+					<label class="control-label" for="website">웹사이트</label>
+					<div class="controls">
+						<input type="text" name="website" value="${user.website}">
+					</div>
+				</div>
+
+				<div class="control-group">
+					<label class="control-label" for="introduce">자기소개</label>
+					<div class="controls">
+						<textarea rows="3" name="introduce">${user.introduce}</textarea>
+					</div>
+				</div>
+
+				<div class="control-group">
+					<label class="control-label" for="profilephoto">프로필 사진</label>
+					<div class="controls">
+						<input type="file" name="profilephoto">
+						<c:if test="${user.profilephoto != null}">
+								<p>등록된 사진 <c:out value="${user.profilephoto}"/> 이 있습니다.</p> 
+						</c:if>
+						<p>사진의 크기는 최대 2MB까지 가능합니다.</p>
+					</div>
+				</div>
+
+				<div class="form-actions">
+					<c:choose>
+						 <c:when test="${method=='POST'}">
+	  						<input type="submit" class="btn btn-primary" value="가입">
+	  					</c:when>
+	  					<c:otherwise>
+	  						<input type="submit" class="btn btn-primary" value="수정">
+	  					</c:otherwise>
+  					</c:choose>
+  					<a href="javascript:history.go(-1);" class="btn btn-danger">취소</a>
+				</div>
+			</fieldset>
+		  </form>
+    </div>
+  </div>
 </body>
 </html>
---
+++ Web_Teamproject/src/dao/MemberDAO.java
@@ -0,0 +1,240 @@
+package dao;
+
+import java.sql.*;
+import javax.naming.*;
+import javax.sql.*;
+
+import bean.Member;
+import bean.PageResult;
+
+
+public class MemberDAO {
+	
+	public static DataSource getDataSource() throws NamingException {
+		Context initCtx = null;
+		Context envCtx = null;
+
+		initCtx = new InitialContext();
+		envCtx = (Context) initCtx.lookup("java:comp/env");
+
+		return (DataSource) envCtx.lookup("jdbc/WebDB");
+	}
+	
+	public static PageResult<Member> getPage(int page, int numItemsInPage) 
+			throws SQLException, NamingException {
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;		
+
+		if (page <= 0) {
+			page = 1;
+		}
+		
+		DataSource ds = getDataSource();
+		PageResult<Member> result = new PageResult<Member>(numItemsInPage, page);
+		
+		
+		int startPos = (page - 1) * numItemsInPage;
+		
+		try {
+			conn = ds.getConnection();
+			stmt = conn.createStatement();
+			
+			// users 테이블: user 수 페이지수 개산
+	 		rs = stmt.executeQuery("SELECT COUNT(*) FROM member");
+			rs.next();
+			
+			result.setNumItems(rs.getInt(1));
+			
+			rs.close();
+			rs = null;
+			stmt.close();
+			stmt = null;
+			
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery("SELECT * FROM member LIMIT " + startPos + ", " + numItemsInPage);
+			
+			while(rs.next()) {
+				result.getList().add(new Member(
+											rs.getString("userid"),
+											rs.getString("userpassword"),
+											rs.getTimestamp("registerdate"),
+											rs.getString("lastname"),
+											rs.getString("firstname"),
+											rs.getString("nickname"),
+											rs.getString("profilephoto"),
+											rs.getString("gender"),
+											rs.getString("email"),
+											rs.getString("introduce"),
+											rs.getString("website"),
+											rs.getInt("level")));
+			}
+		} finally {
+			if (rs != null) try{rs.close();} catch(SQLException e) {}
+			if (stmt != null) try{stmt.close();} catch(SQLException e) {}
+			if (conn != null) try{conn.close();} catch(SQLException e) {}
+		}
+		
+		return result;		
+	}
+	
+	public static Member findById(String userid) throws NamingException, SQLException{
+		Member user = null;
+		
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		DataSource ds = getDataSource();
+		
+		try {
+			conn = ds.getConnection();
+
+			stmt = conn.prepareStatement("SELECT * FROM member WHERE userid = ?");
+			stmt.setString(1, userid);
+			
+			rs = stmt.executeQuery();
+
+			if (rs.next()) {
+				user = new Member(
+						rs.getString("userid"),
+						rs.getString("userpassword"),
+						rs.getTimestamp("registerdate"),
+						rs.getString("lastname"),
+						rs.getString("firstname"),
+						rs.getString("nickname"),
+						rs.getString("profilephoto"),
+						rs.getString("gender"),
+						rs.getString("email"),
+						rs.getString("introduce"),
+						rs.getString("website"),
+						rs.getInt("level"));
+			}	
+		} finally {
+			if (rs != null) try{rs.close();} catch(SQLException e) {}
+			if (stmt != null) try{stmt.close();} catch(SQLException e) {}
+			if (conn != null) try{conn.close();} catch(SQLException e) {}
+		}
+		
+		return user;
+	}
+	
+	public static boolean create(Member user) throws SQLException, NamingException {
+		int result;
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		DataSource ds = getDataSource();
+		try {
+			conn = ds.getConnection();
+
+			stmt = conn.prepareStatement(
+					"INSERT INTO member(userid, userpassword, registerdate, lastname, firstname, nickname, profilephoto, gender, email, introduce, website) " +
+					"VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+					);
+			stmt.setString(1,  user.getUserid());
+			stmt.setString(2,  user.getPwd());
+			stmt.setTimestamp(3,  user.getRegisterdate());
+			stmt.setString(4,  user.getLastname());
+			stmt.setString(5,  user.getFirstname());
+			stmt.setString(6,  user.getNickname());
+			if(user.getProfilephoto() == null) {
+				stmt.setString(7,  "default.gif");
+			} else {
+				stmt.setString(7,  user.getProfilephoto());
+			}
+			stmt.setString(8,  user.getGender());
+			stmt.setString(9,  user.getEmail());
+			stmt.setString(10,  user.getIntroduce());
+			stmt.setString(11,  user.getWebsite());
+			
+			result = stmt.executeUpdate();
+		} finally {
+			if (rs != null) try{rs.close();} catch(SQLException e) {}
+			if (stmt != null) try{stmt.close();} catch(SQLException e) {}
+			if (conn != null) try{conn.close();} catch(SQLException e) {}
+		}
+		
+		return (result == 1);
+	}
+	
+	public static boolean update(Member user) throws SQLException, NamingException {
+		int result;
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		DataSource ds = getDataSource();
+		
+		try {
+			conn = ds.getConnection();
+			
+			// 사진 변경이 없으면
+			if(user.getProfilephoto() == null) {
+				stmt = conn.prepareStatement(
+						"UPDATE member " +
+						"SET  userpassword=?, lastname=?, firstname=?, nickname=?, gender=?, email=?, introduce=?, website=?" +
+						"WHERE userid=?"
+						);
+				stmt.setString(1,  user.getPwd());
+				stmt.setString(2,  user.getLastname());
+				stmt.setString(3,  user.getFirstname());
+				stmt.setString(4,  user.getNickname());
+				stmt.setString(5,  user.getGender());
+				stmt.setString(6,  user.getEmail());
+				stmt.setString(7,  user.getIntroduce());
+				stmt.setString(8,  user.getWebsite());
+				stmt.setString(9,  user.getUserid());
+			} else {
+				stmt = conn.prepareStatement(
+						"UPDATE member " +
+						"SET  userpassword=?, lastname=?, firstname=?, nickname=?, profilephoto=?, gender=?, email=?, introduce=?, website=?" +
+						"WHERE userid=?"
+						);
+				stmt.setString(1,  user.getPwd());
+				stmt.setString(2,  user.getLastname());
+				stmt.setString(3,  user.getFirstname());
+				stmt.setString(4,  user.getNickname());
+				stmt.setString(5,  user.getProfilephoto());
+				stmt.setString(6,  user.getGender());
+				stmt.setString(7,  user.getEmail());
+				stmt.setString(8,  user.getIntroduce());
+				stmt.setString(9,  user.getWebsite());
+				stmt.setString(10,  user.getUserid());
+			}
+			
+			result = stmt.executeUpdate();
+		} finally {
+			if (rs != null) try{rs.close();} catch(SQLException e) {}
+			if (stmt != null) try{stmt.close();} catch(SQLException e) {}
+			if (conn != null) try{conn.close();} catch(SQLException e) {}
+		}
+		
+		return (result == 1);		
+	}
+	
+	public static boolean remove(String userid) throws NamingException, SQLException {
+		int result;
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		DataSource ds = getDataSource();
+		
+		try {
+			conn = ds.getConnection();
+
+			stmt = conn.prepareStatement("DELETE FROM member WHERE userid=?");
+			stmt.setString(1,  userid);
+			
+			result = stmt.executeUpdate();
+		} finally {
+			if (rs != null) try{rs.close();} catch(SQLException e) {}
+			if (stmt != null) try{stmt.close();} catch(SQLException e) {}
+			if (conn != null) try{conn.close();} catch(SQLException e) {}
+		}
+		
+		return (result == 1);		
+	}
+}
